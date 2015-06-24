28b6be6ebd5ac647bb630cb5bd527c2981866558
carsonlevine <carsonglevine@me.com>
2015-06-24 23:46:38 +0000

    Some old code that will be replaced later on.

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,27 @@
 require 'bundler'
 Bundler.require
+require_relative "models/dog.rb"
 
 class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
   end
-
+  
+  post '/' do
+    @dog = Dog.new(params["name"], params["birthday"],params["age"])
+    erb :dogs
+  end
+  
+  get '/dogs' do
+    @fido = Dog.new("Fido", "February 22nd, 2010", 5)
+    erb :dogs
+  end
+  
+  get '/victoria' do
+    erb :victoria
+  end
+  get '/hedgehogs' do
+    erb :hedgehogs
+  end
 end
