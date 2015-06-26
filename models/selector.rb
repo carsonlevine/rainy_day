9edb04b6e551eb193f38bc1275cab73596ba01e8
lucienfd <lucfox10@gmail.com>
2015-06-26 13:50:51 +0000

    updates and fixing merge conflict

--- models/selector.rb
+++ models/selector.rb
@@ -1,35 +1,27 @@
-<<<<<<< HEAD
+
 class Choice
-=======
-class Service
->>>>>>> d6dce70dc3e7a4c4003b7153783cbe21a234d997
+
   attr_accessor :category
   def initialize(category)
     @category = category
   end
 end
 
-<<<<<<< HEAD
-class GenreTV
-=======
+
 class Genre
->>>>>>> d6dce70dc3e7a4c4003b7153783cbe21a234d997
   attr_accessor :tvgenre
   def initialize(tvgenre)
     @tvgenre = tvgenre
   end
 end
 
-<<<<<<< HEAD
-class SubGenreTV
-=======
+
 class SubGenre
->>>>>>> d6dce70dc3e7a4c4003b7153783cbe21a234d997
   attr_accessor :tvsubgenre
   def initialize(tvsubgenre)
     @tvsubgenre = tvsubgenre
   end
-<<<<<<< HEAD
+
 end
 
 class GenreMV
@@ -39,8 +31,4 @@ class GenreMV
   end
 end
 
-# class SubGenreMV
-#   attr_accessor
-=======
-end
->>>>>>> d6dce70dc3e7a4c4003b7153783cbe21a234d997
+
