d408564389d4a94c91d51f3aaa71d8e3fdfb4f70
carsonlevine <carsonglevine@me.com>
2015-06-26 15:05:07 +0000

    hella changes

--- hashes.rb
+++ hashes.rb
@@ -1,7 +1,7 @@
- @tv => {
-    @comedy_tv => {
-        @sitcom_tv =>{
-          :netflix => [
+ @tv = {
+    :comedy_tv => {
+        :sitcom_tv =>{
+          :netflix => {
             :himym => ["How I Met Your Mother", "http://cdn1.nflximg.net/webp/7163/9977163.webp", "http://www.netflix.com/title/70143824"],
             :parks_and_rec => ["Parks and Recreation", "http://cdn1.nflximg.net/webp/7661/11067661.webp", "http://www.netflix.com/title/70143842"],
             :that_70s_show => ["That 70's Show", "http://cdn1.nflximg.net/webp/8033/8358033.webp", "http://www.netflix.com/title/70155610"],
@@ -18,81 +18,80 @@
             :friends => ["F.R.I.E.N.D.S.", "http://cdn0.nflximg.net/webp/3068/9163068.webp", "http://www.netflix.com/title/70153404"],
             :scrubs => ["Scrubs", "http://cdn1.nflximg.net/webp/4627/3754627.webp", "http://www.netflix.com/title/70153393"],
             :grace_and_frankie => ["Grace and Frankie", "http://cdn1.nflximg.net/images/6371/21056371.jpg", "http://www.netflix.com/title/80017537"]
-            ],
-          :hulu => [
+            },
+          :hulu => {
             :workaholics => ["Workaholics", "http://ib2.huluim.com/show/6341?size=220x124&region=us", "http://www.hulu.com/workaholics"],
             :family_guy => ["Family Guy", "http://ib3.huluim.com/show/54?size=220x124&region=us", "http://www.hulu.com/family-guy"],
             :seinfeld => ["Seinfeld", "http://ib1.huluim.com/show/23104?size=220x124&region=us", "http://www.hulu.com/seinfeld"],
             :modern_family => ["Modern Family", "http://ib2.huluim.com/show/2553?size=220x124&region=us", "http://www.hulu.com/modern-family"],
             :the_mindy_project => ["The Mindy Project", "http://ib1.huluim.com/show/11248?size=220x124&region=us", "http://www.hulu.com/the-mindy-project"],
             :blackish => ["black-ish", "http://ib3.huluim.com/show/19738?size=220x124&region=us", "http://www.hulu.com/blackish"],
-            ],
-          :hbogo => [],
-          :prime => [],
-          :other =>[]
+            },
+           :hbogo => [],
+           :prime => [],
+           :other =>[]
           },
-        @talk_show_tv =>{
-          :netflix => [],
-          :hulu => [
+         :talk_show_tv =>{
+           :netflix => [],
+           :hulu => {
             :daily_show => ["The Daily Show with Jon Stewart", "http://ib3.huluim.com/show/902?size=220x124&region=us", "http://www.hulu.com/the-daily-show-with-jon-stewart"],
-            :tonight_show => ["The Tonight Show with Jimmy Fallon", "http://ib1.huluim.com/show/17312?size=220x124&region=us", "http://www.hulu.com/the-tonight-show-starring-jimmy-fallon"],
-            :jimmy_kimmel => ["Jimmy Kimmel Live", "http://ib3.huluim.com/show/2526?size=220x124&region=us", "http://www.hulu.com/jimmy-kimmel-live"],
-            :ellen_show => ["The Ellen Show", "http://ib3.huluim.com/show/6302?size=220x124&region=us", "http://www.hulu.com/the-ellen-show"]
-            ],
-          :hbogo => [
-            :john_oliver => ["Last Week Tonight with John Oliver", "http://go.sky.com/static/SVOD/SKYENTERTAINMENT/IMAGES/Sky%20Atlantic/L/Last%20Week%20Tonight%20With%20John%20Oliver/S2/SE_LastWeekTonightWJO-S2-L.jpg", "http://www.hbogo.com/#search&browseMode=browseGrid?searchTerm=last%20week%20tonight/browse&assetID=GOROSTGP42523?assetType=SERIES?browseMode=browseGrid/"]
-            ],
-          :prime => [],
-          :other =>[]
+             :tonight_show => ["The Tonight Show with Jimmy Fallon", "http://ib1.huluim.com/show/17312?size=220x124&region=us", "http://www.hulu.com/the-tonight-show-starring-jimmy-fallon"],
+             :jimmy_kimmel => ["Jimmy Kimmel Live", "http://ib3.huluim.com/show/2526?size=220x124&region=us", "http://www.hulu.com/jimmy-kimmel-live"],
+             :ellen_show => ["The Ellen Show", "http://ib3.huluim.com/show/6302?size=220x124&region=us", "http://www.hulu.com/the-ellen-show"]
+             },
+         :hbogo => {
+             :john_oliver => ["Last Week Tonight with John Oliver", "http://go.sky.com/static/SVOD/SKYENTERTAINMENT/IMAGES/Sky%20Atlantic/L/Last%20Week%20Tonight%20With%20John%20Oliver/S2/SE_LastWeekTonightWJO-S2-L.jpg", "http://www.hbogo.com/#search&browseMode=browseGrid?searchTerm=last%20week%20tonight/browse&assetID=GOROSTGP42523?assetType=SERIES?browseMode=browseGrid/"]
+            },
+           :prime => [],
+           :other =>[]
           },
-       @satire_tv => {
-         :netflix => [
-            :psych => ["Psych", "http://cdn0.nflximg.net/webp/8136/11948136.webp", "http://www.netflix.com/title/70153394"],
-            :bms => ["Blue Mountain State", "http://cdn1.nflximg.net/webp/1587/3431587.webp", "http://www.netflix.com/title/70157491"],
-            :white_collar => ["White Collar", "http://cdn0.nflximg.net/webp/1890/3511890.webp", "http://www.netflix.com/title/70166091"],
-            :californication => ["Californication", "http://cdn0.nflximg.net/webp/8780/11318780.webp", "http://www.netflix.com/title/70140366"],
-            :boondocks => ["The Boondocks", "http://cdn0.nflximg.net/webp/6792/4216792.webp", "http://www.netflix.com/title/70153391"],
-            :freaks_and_geeks => ["Freaks and Geeks", "http://cdn0.nflximg.net/webp/4990/3454990.webp", "http://www.netflix.com/title/70253797"],
-            :unbreakable_kimmy => ["Unbreakable Kimmy Schmidt", "http://cdn0.nflximg.net/images/8054/12138054.jpg", "http://www.netflix.com/title/80025384"],
-            :chuck => ["Chuck", "http://cdn1.nflximg.net/webp/7751/4107751.webp", "http://www.netflix.com/title/70157475"],
-            :louie => ["Louie", "http://cdn1.nflximg.net/webp/3063/13013063.webp", "http://www.netflix.com/title/70179977"],
-            :glee => ["Freaks and Geeks", "http://cdn0.nflximg.net/webp/8454/3748454.webp", "http://www.netflix.com/title/70143843"]
-            ],
-          :hulu => [],
-          :hbogo => [
-            :entourage => ["Entourage", "http://ecx.images-amazon.com/images/I/51cq77MONeL._PI_PJStripe-HD-Only-500px,TopLeft,0,0_AA160_.jpg", "http://www.hbogo.com/#series/browse&assetID=GORO1D595?assetType=SERIES?browseMode=browseGrid?browseID=category.INDB464/"],
-            :curb_your_enthusiasm => ["Curb Your Enthusiasm", "http://ecx.images-amazon.com/images/I/51F7MKfqqGL._AA160_.jpg", "http://www.hbogo.com/#search&browseMode=browseGrid?searchTerm=curb%20your%20enthus/browse&assetID=GORO1D602?assetType=SERIES?browseMode=browseGrid/"],
-            :jamie_private_school => ["Ja'mie Private School Girl", "http://ecx.images-amazon.com/images/I/41eYwJLevmL._PI_PJStripe-HD-Only-500px,TopLeft,0,0_AA160_.jpg", "http://www.hbogo.com/#search&browseMode=browseGrid?searchTerm=ja'/browse&assetID=GOROSTGP41769?assetType=SERIES?browseMode=browseGrid/"],
-            :silicon_valley => ["Silicon Valley", "http://ecx.images-amazon.com/images/I/51pvg%2B-SBBL._PI_PJStripe-HD-Only-500px,TopLeft,0,0_AA160_.jpg", "http://www.hbogo.com/#search&browseMode=browseGrid?searchTerm=silicon/browse&assetID=GOROSTGP42372?assetType=SERIES?browseMode=browseGrid/"],
-            :veep => ["Veep", "http://ecx.images-amazon.com/images/I/51fevasyOrL._PI_PJStripe-HD-Only-500px,TopLeft,0,0_AA160_.jpg", "http://www.hbogo.com/#search&browseMode=browseGrid?searchTerm=veep/browse&assetID=GOROSTGP35040?assetType=SERIES?browseMode=browseGrid/"],
-            :sex_and_the_city => ["Sex and the City", "http://ecx.images-amazon.com/images/I/51gQ%2Ble1WPL._AA160_.jpg", "http://www.hbogo.com/#search&browseMode=browseGrid?searchTerm=sex/browse&assetID=GORO1D598?assetType=SERIES?browseMode=browseGrid/"]
-            ],
-          :prime => [],
-          :other =>[]
-      }
-      @sketch_tv => {
-          :netflix => [
-            :portlandia => ["Portlandia", "http://cdn1.nflximg.net/webp/4569/11344569.webp", "http://www.netflix.com/title/70185015"]
-            ],
-          :hulu => [
-            :key_and_peele => ["Key and Peele", "http://ib3.huluim.com/show/8898?size=220x124&region=us", "http://www.hulu.com/key-and-peele"],
+        :satire_tv => {
+          :netflix => {
+             :psych => ["Psych", "http://cdn0.nflximg.net/webp/8136/11948136.webp", "http://www.netflix.com/title/70153394"],
+             :bms => ["Blue Mountain State", "http://cdn1.nflximg.net/webp/1587/3431587.webp", "http://www.netflix.com/title/70157491"],
+             :white_collar => ["White Collar", "http://cdn0.nflximg.net/webp/1890/3511890.webp", "http://www.netflix.com/title/70166091"],
+             :californication => ["Californication", "http://cdn0.nflximg.net/webp/8780/11318780.webp", "http://www.netflix.com/title/70140366"],
+             :boondocks => ["The Boondocks", "http://cdn0.nflximg.net/webp/6792/4216792.webp", "http://www.netflix.com/title/70153391"],
+             :freaks_and_geeks => ["Freaks and Geeks", "http://cdn0.nflximg.net/webp/4990/3454990.webp", "http://www.netflix.com/title/70253797"],
+             :unbreakable_kimmy => ["Unbreakable Kimmy Schmidt", "http://cdn0.nflximg.net/images/8054/12138054.jpg", "http://www.netflix.com/title/80025384"],
+             :chuck => ["Chuck", "http://cdn1.nflximg.net/webp/7751/4107751.webp", "http://www.netflix.com/title/70157475"],
+             :louie => ["Louie", "http://cdn1.nflximg.net/webp/3063/13013063.webp", "http://www.netflix.com/title/70179977"],
+             :glee => ["Freaks and Geeks", "http://cdn0.nflximg.net/webp/8454/3748454.webp", "http://www.netflix.com/title/70143843"]
+           },
+           :hulu => [],
+           :hbogo => {
+             :entourage => ["Entourage", "http://ecx.images-amazon.com/images/I/51cq77MONeL._PI_PJStripe-HD-Only-500px,TopLeft,0,0_AA160_.jpg", "http://www.hbogo.com/#series/browse&assetID=GORO1D595?assetType=SERIES?browseMode=browseGrid?browseID=category.INDB464/"],
+             :curb_your_enthusiasm => ["Curb Your Enthusiasm", "http://ecx.images-amazon.com/images/I/51F7MKfqqGL._AA160_.jpg", "http://www.hbogo.com/#search&browseMode=browseGrid?searchTerm=curb%20your%20enthus/browse&assetID=GORO1D602?assetType=SERIES?browseMode=browseGrid/"],
+             :jamie_private_school => ["Ja'mie Private School Girl", "http://ecx.images-amazon.com/images/I/41eYwJLevmL._PI_PJStripe-HD-Only-500px,TopLeft,0,0_AA160_.jpg", "http://www.hbogo.com/#search&browseMode=browseGrid?searchTerm=ja'/browse&assetID=GOROSTGP41769?assetType=SERIES?browseMode=browseGrid/"],
+             :silicon_valley => ["Silicon Valley", "http://ecx.images-amazon.com/images/I/51pvg%2B-SBBL._PI_PJStripe-HD-Only-500px,TopLeft,0,0_AA160_.jpg", "http://www.hbogo.com/#search&browseMode=browseGrid?searchTerm=silicon/browse&assetID=GOROSTGP42372?assetType=SERIES?browseMode=browseGrid/"],
+             :veep => ["Veep", "http://ecx.images-amazon.com/images/I/51fevasyOrL._PI_PJStripe-HD-Only-500px,TopLeft,0,0_AA160_.jpg", "http://www.hbogo.com/#search&browseMode=browseGrid?searchTerm=veep/browse&assetID=GOROSTGP35040?assetType=SERIES?browseMode=browseGrid/"],
+             :sex_and_the_city => ["Sex and the City", "http://ecx.images-amazon.com/images/I/51gQ%2Ble1WPL._AA160_.jpg", "http://www.hbogo.com/#search&browseMode=browseGrid?searchTerm=sex/browse&assetID=GORO1D598?assetType=SERIES?browseMode=browseGrid/"]
+             },
+           :prime => [],
+           :other =>[]
+      },
+       :sketch_tv => {
+           :netflix => {
+             :portlandia => ["Portlandia", "http://cdn1.nflximg.net/webp/4569/11344569.webp", "http://www.netflix.com/title/70185015"]
+             },
+           :hulu => {
+             :key_and_peele => ["Key and Peele", "http://ib3.huluim.com/show/8898?size=220x124&region=us", "http://www.hulu.com/key-and-peele"],
             :snl => ["Saturday Night Live", "http://ib2.huluim.com/show/77?size=220x124&region=us", "http://www.hulu.com/saturday-night-live"],
-            ],
-            :amy_schumer => ["Inside Amy Schumer", "http://ib4.huluim.com/show/14027?size=220x124&region=us", "http://www.hulu.com/inside-amy-schumer"],
-            :kroll_show => ["Kroll Show", "http://ib2.huluim.com/show/12837?size=220x124&region=us", "http://www.hulu.com/kroll-show"],
+             :amy_schumer => ["Inside Amy Schumer", "http://ib4.huluim.com/show/14027?size=220x124&region=us", "http://www.hulu.com/inside-amy-schumer"],
+             :kroll_show => ["Kroll Show", "http://ib2.huluim.com/show/12837?size=220x124&region=us", "http://www.hulu.com/kroll-show"],
             :loiter_squad => ["Loiter Squad", "http://ib2.huluim.com/show/77?size=220x124&region=us", "http://www.hulu.com/saturday-night-live"]
-            ],
-          :hbogo => [],
+             },
+           :hbogo => [],
           :prime => [],
-          :other =>[]
-      }
-    }
-   @drama_tv => {
+           :other =>[]
+      },
+    },
+    :drama_tv => {
           :netflix => [],
           :hulu => [],
-          :hbogo => [],
-          :prime => [],
+           :hbogo => [],
+           :prime => [],
           :other =>[]
-     }
+      },
 
 }
--- models/selector.rb
+++ models/selector.rb
@@ -1,12 +1,9 @@
-
 class Choice
-
   attr_accessor :category
   def initialize(category)
     @category = category
   end
 end
-
 
 class Genre
   attr_accessor :tvgenre
@@ -15,20 +12,9 @@ class Genre
   end
 end
 
-
-class SubGenre
+class SubGenreTV
   attr_accessor :tvsubgenre
   def initialize(tvsubgenre)
     @tvsubgenre = tvsubgenre
   end
-
 end
-
-class GenreMV
-  attr_accessor :mvgenre
-  def initialize(mvgenre)
-    @mvgenre = mvgenre
-  end
-end
-
-
